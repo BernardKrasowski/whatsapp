1266012fe6f3ed5a79e5c0e9234f24677541fee6
Bernard <bernardkrasowski111@gmail.com>
2021-07-25 15:31:09 +0100

    first Commit whats upp

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBm-hPlMEfAjYoDWXSyyr8W0bWE9I5eb9A",
+  authDomain: "whatsapp-4e905.firebaseapp.com",
+  projectId: "whatsapp-4e905",
+  storageBucket: "whatsapp-4e905.appspot.com",
+  messagingSenderId: "759701022593",
+  appId: "1:759701022593:web:58f82d4111358992ed2a80"
+};
