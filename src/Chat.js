afddffa136db285981297e754a23d8daf34b41ac
Bernard <bernardkrasowski111@gmail.com>
2021-07-25 17:56:05 +0100

    ready front

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core'
+import React, { useState } from 'react'
 import './Chat.css'
+import SearchOutlined from '@material-ui/icons/Search'
+import AttachFile from '@material-ui/icons/AttachFile'
+import MoreVert from '@material-ui/icons/MoreVert'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import SelectInput from '@material-ui/core/Select/SelectInput'
+import MicIcon from '@material-ui/icons/Mic'
 
 function Chat() {
+  const [input, setInput] = useState('')
+  const sendMessage = () => { }
   return (
     <div className='chat'>
-      <h1>chat</h1>
+      <div className="chat__header">
+
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Bernard</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Bernard</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form >
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder='Type a message' />
+
+          <button onClick={sendMessage}
+            type='submit'>
+            Send Message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+
     </div>
   )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import './Sidebar.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { SearchOutlined } from "@material-ui/icons"
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1> sidebar </h1>
+
       <div className="sidebar__header">
-        <div className="sidebar__headerRight"></div>
+        <Avatar src='https://scontent-lcy1-1.xx.fbcdn.net/v/t1.6435-9/178103485_3838936436190484_3596619077807659465_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=FzPZfbC_tJgAX-qE4Mt&tn=zdr7KTmz1wQj7c-0&_nc_ht=scontent-lcy1-1.xx&oh=a9abf1a92f6430deabfc53b365caf948&oe=6122E442' />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+
+
     </div>
   )
 }
